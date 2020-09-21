d107b641b6eafcfbff23f89c93cc24e9634af485
admin <tranhailong1998@gmail.com>
2020-09-21 11:50:23 +0700

    a

--- Formula/espanso.rb
+++ Formula/espanso.rb
@@ -2,7 +2,7 @@ class Espanso < Formula
   desc "Cross-platform Text Expander written in Rust"
   homepage "https://github.com/federico-terzi/espanso"
   url "https://github.com/federico-terzi/espanso/releases/latest/download/espanso-mac.tar.gz"
-  sha256 "f96aaba9d5908a2fcd2f878730916c73031e2870241ca31c6316f612e39ec506"
+  sha256 "2473866b99eef9ea983200b7aac91592b938404ffaa1fb8c72beacb2ebd3203a"
   version "0.1.0"
 
   def install
